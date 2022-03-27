926f06d959a3640f7f69b432c706cc5fe910758f
Jon Thielen <github@thielen.science>
2022-03-27 08:02:30 -0600

    whoops, forgot uwsgi, very important

--- setup_conda_environment.sh
+++ setup_conda_environment.sh
@@ -1,4 +1,4 @@
 conda config --add channels conda-forge
 conda config --set channel_priority strict
-conda create -n chase python pandas pyproj requests flask flask-restful numpy pytz boto3
+conda create -n chase python pandas pyproj requests flask flask-restful numpy pytz boto3 uwsgi
 conda activate chase
